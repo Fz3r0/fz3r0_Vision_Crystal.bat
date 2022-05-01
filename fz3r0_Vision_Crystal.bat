5a3cb6a54a958a90573b074371c00038043e5cff
Fz3r0 <94720207+Fz3r0@users.noreply.github.com>
2022-05-01 10:22:22 -0500

    Create fz3r0_Vision_Crystal.bat

---
+++ fz3r0_Vision_Crystal.bat
@@ -0,0 +1,139 @@
+## Still missing some OS!!!! no mames!!! 
+
+@echo off
+title huevos
+mode con:cols=64 lines=59
+
+setlocal
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+::if "%version%" == "6.3" echo Windows 8.1
+::if "%version%" == "6.2" echo Windows 8.
+::if "%version%" == "6.1" echo Windows 7.
+::if "%version%" == "6.0" echo Windows Vista.
+::if "%version%" == "10.0" echo Windows 10.
+::echo %version%
+rem etc etc
+
+
+pause
+
+
+echo       __.,-*~*-,._.,-* fz3r0.Ghost1 *-,._.,-*~*-,.__    
+echo     ./                                              \.  
+echo   ./    *~._.,-*~["||== MASTER-MIND ==||]~*-,._.~*    \.
+echo "                                                       " 
+echo "                        `-.`'.-'                       " 
+echo "                     `-.        .-'.                   " 
+echo "                  `-.    -./\.-    .-'                 " 
+echo "                      -.  /_|\  .-                     " 
+echo "                  `-.   `/____\'   .-'.                " 
+echo "               `-.    -./.-""-.\.-      '              " 
+echo "                  `-.  /< (()) >\  .-'                 " 
+echo "                -   .`/__`-..-'__\'   .-               " 
+echo "              ,...`-./___|____|___\.-'.,.              " 
+echo "                 ,-'   ,` . . ',   `-,                 " 
+echo "              ,-'   ________________  `-,              " 
+echo "                 ,'/____|_____|_____\                  " 
+echo "                / /__|_____|_____|___\                 " 
+echo "               / /|_____|_____|_____|_\                " 
+echo "              ' /____|_____|_____|_____\               " 
+echo "            .' /__|_____|_____|_____|___\              " 
+echo "           ,' /|_____|_____|_____|_____|_\             " 
+echo "         . ../____|_____|_____|_____|_____\            " 
+echo "          ''/__|_____|_____|_____|_____|___\           " 
+echo "        /'./______|_____|_____|_____|_____|_\          " 
+echo "       \:./____|_____|_____|_____|_____|_____\         " 
+echo "        \/___|____|_____|_____|_____|_____|___\        " 
+echo "         """"""""""""""""""""""""""""""""""""""        " 
+echo "                                                       "
+echo "   I will search deep into the System Information...   "
+
+timeout /t 3 /nobreak > nul
+echo                           . . .
+timeout /t 2 /nobreak > nul
+echo                           . . .  
+timeout /t 2 /nobreak > nul
+echo                         . . . . . 
+timeout /t 2 /nobreak > nul
+
+echo "                                                       " 
+
+if "%version%" == "10.0" echo "      [TARGET SYSTEM: WINDOWS 10 - VERSION %version%]       "
+
+endlocal
+
+
+timeout /t 4 /nobreak > nul
+echo "                                                       "
+echo "    That is a very easy Operating System to read...    "
+timeout /t 4 /nobreak > nul
+echo "                                                       "
+echo "   I have collected ALL it's Information Succesfully   "
+timeout /t 3 /nobreak > nul
+echo "                                                       "
+pause
+timeout /t 1 /nobreak > nul
+echo                           . . .
+timeout /t 1 /nobreak > nul
+echo                           . . .  
+timeout /t 1 /nobreak > nul
+echo                           . . . 
+timeout /t 1 /nobreak > nul
+echo                         . . . . . 
+timeout /t 2 /nobreak > nul
+
+
+echo "                                                       "
+echo "          And now...Let me read your mind...           "
+echo "                                                       "
+                                                      
+
+timeout /t 3 /nobreak > nul
+echo "       Yes...I will search deep into your Soul...      " 
+
+timeout /t 3 /nobreak > nul
+echo                           . . .
+timeout /t 1 /nobreak > nul
+echo                           . . .  
+timeout /t 1 /nobreak > nul
+echo                           . . . 
+timeout /t 1 /nobreak > nul
+echo                         . . . . . 
+timeout /t 2 /nobreak > nul
+
+:: se le suman 2 al último número, por eso existe el -1 y el 0. Si fueran 100 respuestas sería 102 - 1
+set /a number=%RANDOM% %% 21 - 1
+
+
+echo "                                                       "
+echo =========================================================
+echo "                                                       "
+ECHO %number%   
+if "%number%" == "-1" echo "                 YOU ARE SO NEGATIVE                   "
+if "%number%" == "0" echo "                      EMPTYNESS...                     "
+if "%number%" == "1" echo "                  YOU WILL DIE ALONE                   "
+if "%number%" == "2" echo "           YOU ARE THROWING YOUR LIFE AWAY             "
+if "%number%" == "3" echo "                  THINK FOR YOURSELF                   "
+if "%number%" == "4" echo "                QUESTION AUTHORITY 
+if "%number%" == "5" echo "           MEET STRANGERS WITHOUT PREJUDICE 
+if "%number%" == "6" echo "         YES...POPLE ARE MEAN AND DESTRUCTIVE
+if "%number%" == "7" echo "                 YOU ARE DARK INSIDE                    "   
+if "%number%" == "8" echo "                   TRUST GOOD PEOPLE 
+if "%number%" == "9" echo "       THERE IS NO PATH... AND YET YOU SEEK IT...       "
+if "%number%" == "10" echo "               MAY YOUR TROUBLES BE MANY               "
+if "%number%" == "11" echo "       YOU CAN'T RUN AWAY FROM YOUR PROBLEMS           "
+if "%number%" == "12" echo "   YOUR SUN...IT IS SETTING...IT IS GETTING DARK       "
+if "%number%" == "13" echo "       Do not spend your life thinking that "god"      "
+if "%number%" == "13" echo "        Has to solve always your dumb, lazy life       "
+if "%number%" == "14" echo "                   CONNECT THE DOTS!                   "
+if "%number%" == "15" echo "      LIFE IS HARD HUH?... MAN UP AND KEEP GOING       "
+if "%number%" == "16" echo "          YOU WILL DIE, ENJOY THIS WEEKEND             "
+if "%number%" == "17" echo "           ARE YOU SURE THE ONLY YOU IS YOU?           "
+if "%number%" == "18" echo "         THERE IS A MONSTER INSIDE OF YOU              "
+if "%number%" == "19" echo "   YOU NEED TO DO MORE PHYSICAL EXCERCISE FATTIE       "
+echo "                                                       "
+echo =========================================================
+echo "                                                       "
+                                
+
+pause
